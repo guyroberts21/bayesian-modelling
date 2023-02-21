bd1b95c3ad557baade5baec5c08b982b120fc717
guyroberts21 <guyroberts21@outlook.com>
2023-02-21 20:04:23 +0000

    bug fix

--- assignments/assignment6/question_code.R
+++ assignments/assignment6/question_code.R
@@ -1,10 +1,5 @@
-compound_dist <- function(N, alpha) {
-  numerator <- stats::beta(N+1, alpha+1)
-  denominator <- stats::beta(alpha, 1) * factorial(N + alpha)
-  return(numerator/denominator)
+compound_dist = function(N, alpha) {
+  return(beta(alpha+1, N+1) / beta(alpha, 1))
 }
 
 compound_dist(2, 1)
-
-# manual test
-(factorial(N + alpha) / (factorial(N)*factorial(alpha))) * (N+1)/(alpha+N+1)
